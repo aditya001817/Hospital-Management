a1a931d028716a83bb3b551a7c0817430e460713
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-09 21:32:01 +0530

    updated patient

--- src/main/java/com/hospitalmanagement/models/Patient.java
+++ src/main/java/com/hospitalmanagement/models/Patient.java
@@ -3,8 +3,8 @@ package com.hospitalmanagement.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Builder
