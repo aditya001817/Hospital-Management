18c360270ee4e8bb7e476294b475d3ecff16a3d8
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-10 14:21:18 +0530

    updated doctor

---
+++ src/main/java/com/hospitalmanagement/entity/Bill.java
@@ -0,0 +1,25 @@
+package com.hospitalmanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long patientid;
+    private double amount;
+    private long doctorId;
+    private String date;
+    private String patientname;
+}
