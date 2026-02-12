09b7e9841c816b723bad9b0aedc83f94533969a2
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-12 21:45:06 +0530

    updated patient  Service

--- src/main/java/com/hospitalmanagement/controller/PatientController.java
+++ src/main/java/com/hospitalmanagement/controller/PatientController.java
@@ -1,6 +1,8 @@
 package com.hospitalmanagement.controller;
 
 import com.hospitalmanagement.entity.Patient;
+import com.hospitalmanagement.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,31 +11,36 @@ import java.util.List;
 @RequestMapping("/api/v1/patient")
 public class PatientController {
 
+    @Autowired
+    private PatientService patientService;
+
     @GetMapping("/all")
     public List<Patient> getAllPatients() {
         System.out.println("Getting all patients");
-        return null;
+        return patientService.getAllPatients();
     }
 
     @GetMapping("/{id}")
     public Patient getPatientById(@PathVariable Long id) {
         System.out.println("Getting patient by id: "+id);
-        return null;
+        return patientService.getPatientById(id);
     }
 
     @PostMapping("/create")
     public Patient createPatient(@RequestBody Patient patient) {
         System.out.println("Creating a new patient");
-        return patient;
+        return patientService.createPatient(patient);
     }
 
     @PutMapping("/update/{id}")
     public void updatePatient(@PathVariable Long id) {
         System.out.println("Updating patient "+id);
+        patientService.updatePatient(id);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deletePatient(@PathVariable Long id) {
         System.out.println("Deleting patient "+id);
+        patientService.deletePatient(id);
     }
 }
--- src/main/java/com/hospitalmanagement/service/PatientService.java
+++ src/main/java/com/hospitalmanagement/service/PatientService.java
@@ -1,5 +1,58 @@
 package com.hospitalmanagement.service;
 
+import com.hospitalmanagement.entity.Patient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Service
 public class PatientService {
 
+    public List<Patient> getAllPatients() {
+        try {
+            System.out.println("Into the service layer");
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+
+    public Patient getPatientById(Long id) {
+        try {
+            System.out.println("Into the service layer");
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public Patient createPatient(Patient patient) {
+        try {
+            System.out.println("Into the service layer");
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void updatePatient(Long id) {
+        try {
+            System.out.println("Into the service layer");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deletePatient(Long id) {
+        try {
+            System.out.println("Into the service layer");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
