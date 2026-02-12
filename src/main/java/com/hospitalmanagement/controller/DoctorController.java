5d9c63ae9c96cfca03c9f31c3fbf5375fde83f75
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-12 21:54:21 +0530

    updated Doctor Service

--- src/main/java/com/hospitalmanagement/controller/DoctorController.java
+++ src/main/java/com/hospitalmanagement/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package com.hospitalmanagement.controller;
 
 import com.hospitalmanagement.entity.Doctor;
+import com.hospitalmanagement.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLOutput;
@@ -11,31 +13,36 @@ import java.util.List;
 @RequestMapping("/api/v1/doctor")
 public class DoctorController {
 
+    @Autowired
+    DoctorService doctorService;
+
     @GetMapping("/all")
     public List<Doctor> getAllDoctors() {
         System.out.println("Getting Doctors");
-        return null;
+        return doctorService.getAllDoctors();
     }
 
     @GetMapping("/{id}")
     public Doctor getDoctorById(@PathVariable Long id) {
         System.out.println("Getting Doctor by id");
-        return null;
+        return doctorService.getDoctorById(id);
     }
 
     @PostMapping("/create")
     public Doctor createDoctor(@RequestBody Doctor doctor) {
         System.out.println("Creating Doctor");
-        return doctor;
+        return doctorService.createDoctor(doctor);
     }
 
     @PutMapping("/update/{id}")
     public void updateDoctor(@PathVariable Long id) {
         System.out.println("Updating Doctor "+id);
+        doctorService.updateDoctor(id);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteDoctor(@PathVariable Long id) {
         System.out.println("Deleting Doctor "+id);
+        doctorService.deleteDoctor(id);
     }
 }
--- src/main/java/com/hospitalmanagement/service/DoctorService.java
+++ src/main/java/com/hospitalmanagement/service/DoctorService.java
@@ -1,5 +1,34 @@
 package com.hospitalmanagement.service;
 
+import com.hospitalmanagement.entity.Doctor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Service
 public class DoctorService {
 
+    public List<Doctor> getAllDoctors() {
+        System.out.println("Into service layer Doctors");
+        return null;
+    }
+
+    public Doctor getDoctorById(Long id) {
+        System.out.println("Into service layer Doctors");
+        return null;
+    }
+
+    public Doctor createDoctor(Doctor doctor) {
+        System.out.println("Into service layer Doctors");
+        return null;
+    }
+
+    public void updateDoctor(Long id) {
+        System.out.println("Into service layer Doctors");
+    }
+
+    public void deleteDoctor(Long id) {
+        System.out.println("Into service layer Doctors");
+    }
 }
